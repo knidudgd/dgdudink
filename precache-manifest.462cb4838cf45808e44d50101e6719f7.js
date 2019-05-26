1cad2eab54093ee9f29ad1b1914a8ab22c387c6a
Buna <31736483+butatensei@users.noreply.github.com>
2019-05-26 10:39:41 +0200

    Updates

---
+++ precache-manifest.462cb4838cf45808e44d50101e6719f7.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "ba8a49904bc934e4f7137268e9ee4f3a",
+    "url": "/knidudgd/dgdudink/static/media/Proj2Pic.ba8a4990.png"
+  },
+  {
+    "revision": "fce99715765aa52be419",
+    "url": "/knidudgd/dgdudink/static/css/main.3bf5a0f5.chunk.css"
+  },
+  {
+    "revision": "6ce2d82d9c02498312fb",
+    "url": "/knidudgd/dgdudink/static/js/runtime~main.94c97d9e.js"
+  },
+  {
+    "revision": "90b2835045675420f99005fa3e22c872",
+    "url": "/knidudgd/dgdudink/static/media/TemporaryPic.90b28350.png"
+  },
+  {
+    "revision": "3a2b056e91df41982157",
+    "url": "/knidudgd/dgdudink/static/js/2.a9839802.chunk.js"
+  },
+  {
+    "revision": "7fb99744eaccae943d5adf537b5b3a93",
+    "url": "/knidudgd/dgdudink/static/media/LinkedInLogo.7fb99744.png"
+  },
+  {
+    "revision": "fce99715765aa52be419",
+    "url": "/knidudgd/dgdudink/static/js/main.9259a266.chunk.js"
+  },
+  {
+    "revision": "99b6d27482804f40580c20b655b8c779",
+    "url": "/knidudgd/dgdudink/static/media/Proj3Pic.99b6d274.png"
+  },
+  {
+    "revision": "477dfcfc997d3c5684247ab3eb50b663",
+    "url": "/knidudgd/dgdudink/static/media/ProjGLPic.477dfcfc.png"
+  },
+  {
+    "revision": "a05a1abb80da689740feef7bc63a1919",
+    "url": "/knidudgd/dgdudink/static/media/Proj4Pic.a05a1abb.png"
+  },
+  {
+    "revision": "3a2b056e91df41982157",
+    "url": "/knidudgd/dgdudink/static/css/2.3f54c157.chunk.css"
+  },
+  {
+    "revision": "adba3536be8bb46d90f9a3d920925bf5",
+    "url": "/knidudgd/dgdudink/index.html"
+  }
+];
